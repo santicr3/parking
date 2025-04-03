8cbf8251406b8331fd1120ac4b9d617ea0b4d6d8
Santicr3 <Santicr3@gmail.com>
2025-04-03 20:25:57 +0200

    General Refactor for a clean structure

--- frontend/main.py
+++ frontend/main.py
@@ -1,31 +1,71 @@
 import flet as ft
-
+import client
+import admin
 def main(page: ft.Page):
-    page.title = "Hello world with Flet!!"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    file_picker = ft.FilePicker()
-    page.overlay.append(file_picker)
-    page.update()
-    
-    def upload_files(e):
-        upload_list = []
-        if file_picker.result != None and file_picker.result.files != None:
-            for f in file_picker.result.files:
-                upload_list.append(
-                    FilePickerUploadFile(
-                        f.name,
-                        upload_url=page.get_upload_url(f.name, 600),
-                    )
+    def pick_files_result(e: ft.FilePickerResultEvent):
+        selected_files.value = (
+            ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
+        )
+        selected_files.update()
+
+    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
+    selected_files = ft.Text()
+
+    page.overlay.append(pick_files_dialog)
+
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+
+    root_page = ft.Container(content=
+        ft.Column(
+            [
+                ft.ElevatedButton(
+                    "Admin",
+                    icon=ft.Icons.ADMIN_PANEL_SETTINGS_OUTLINED,
+                    on_click=lambda _: change_route(page, '/admin'),
+                ),
+
+                ft.ElevatedButton(
+                    "Client",
+                    icon=ft.Icons.PERSON_2_OUTLINED,
+                    on_click=lambda _: change_route(page, '/client'),
+                ),
+            ],
+        ),
+        )
+
+    def change_route(page: ft.Page, new_route):
+        page.route = new_route
+        print(page.route)
+        page.views.clear()
+        if page.route == "/":
+            page.views.append(
+                ft.View(
+                    "/",
+                    [
+                        root_page
+                    ],
                 )
-            file_picker.upload(upload_list)
+            )
+        if page.route == "/client":
+            page.views.append(
+                ft.View(
+                    "/client",
+                    [
+                        client.get_client(page)
+                    ],
+                )
+            )
+        if page.route == "/admin":
+            page.views.append(
+                ft.View(
+                    "/admin",
+                    [
+                        client.get_admin(page)
+                    ],
+                )
+            )
+        page.update()
 
-    chose_file_button = ft.ElevatedButton("Upload", on_click=upload_files)
-
-    page.add(chose_file_button)
-
-    page.update()
-
-
-ft.app(target=main)
+ft.app(main)
