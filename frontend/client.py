d0ccad609aafd6ed49dcac8f4569a2f3d43e4320
Santicr3 <Santicr3@gmail.com>
2025-04-09 19:59:14 +0200

    Frontend improvment

--- frontend/client.py
+++ frontend/client.py
@@ -1,37 +1,34 @@
 import flet as ft
 
 def get_client(page: ft.Page):
-    def pick_files_result(e: ft.FilePickerResultEvent):
+
+    selected_files = ft.Text()
+
+    async def pick_files_result(e: ft.FilePickerResultEvent):
+        print(e)
         selected_files.value = (
             ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
         )
         selected_files.update()
 
+
     pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
-    selected_files = ft.Text()
+    if pick_files_dialog not in page.overlay:
+        page.overlay.append(pick_files_dialog)
 
-    page.overlay.append(pick_files_dialog)
-
-    page.add(
-        ft.Column(
-            [
-                ft.ElevatedButton(
-                    "Coche entra",
-                    icon=ft.Icons.ARROW_CIRCLE_UP,
-                    on_click=lambda _: pick_files_dialog.pick_files(
-                        allow_multiple=True
-                    ),
-                ),
-
-                ft.ElevatedButton(
-                    "Coche sale",
-                    icon=ft.Icons.ARROW_CIRCLE_DOWN,
-                    on_click=lambda _: pick_files_dialog.pick_files(
-                        allow_multiple=True
-                    ),
-                ),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
+    return ft.Column(
+        [
+            ft.ElevatedButton(
+                "Coche entra",
+                icon=ft.Icons.ARROW_CIRCLE_UP,
+                on_click=lambda _: pick_files_dialog.pick_files(),
+            ),
+            ft.ElevatedButton(
+                "Coche sale",
+                icon=ft.Icons.ARROW_CIRCLE_DOWN,
+                on_click=lambda _: pick_files_dialog.pick_files(),
+            ),
+            selected_files,
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
     )
-
--- frontend/main.py
+++ frontend/main.py
@@ -1,71 +1,63 @@
 import flet as ft
 import client
 import admin
+
 def main(page: ft.Page):
 
-    def pick_files_result(e: ft.FilePickerResultEvent):
-        selected_files.value = (
-            ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
-        )
-        selected_files.update()
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
-    selected_files = ft.Text()
-
-    page.overlay.append(pick_files_dialog)
-
-    page.vertical_alignment=ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
-
-    root_page = ft.Container(content=
-        ft.Column(
+    root_page = ft.Container(
+        content=ft.Column(
             [
                 ft.ElevatedButton(
                     "Admin",
                     icon=ft.Icons.ADMIN_PANEL_SETTINGS_OUTLINED,
-                    on_click=lambda _: change_route(page, '/admin'),
+                    on_click=lambda _: page.go("/admin"),
                 ),
-
                 ft.ElevatedButton(
                     "Client",
                     icon=ft.Icons.PERSON_2_OUTLINED,
-                    on_click=lambda _: change_route(page, '/client'),
+                    on_click=lambda _: page.go("/client"),
                 ),
             ],
         ),
-        )
+    )
 
-    def change_route(page: ft.Page, new_route):
-        page.route = new_route
-        print(page.route)
+    def change_route(e: ft.RouteChangeEvent):
         page.views.clear()
-        if page.route == "/":
+
+        route = e.route
+        print("Ruta actual:", route)
+
+        if route == "/":
             page.views.append(
                 ft.View(
                     "/",
-                    [
-                        root_page
-                    ],
+                    [root_page],
                 )
             )
-        if page.route == "/client":
+        elif route == "/client":
             page.views.append(
                 ft.View(
                     "/client",
-                    [
-                        client.get_client(page)
-                    ],
+                    [client.get_client(page)],
                 )
             )
-        if page.route == "/admin":
+        elif route == "/admin":
+            print("Route: Admin")
             page.views.append(
                 ft.View(
                     "/admin",
-                    [
-                        client.get_admin(page)
-                    ],
+                    [client.get_client(page)],
                 )
             )
+
         page.update()
+
+    page.on_route_change = change_route
+
+    page.go(page.route)
+
 
 ft.app(main)
